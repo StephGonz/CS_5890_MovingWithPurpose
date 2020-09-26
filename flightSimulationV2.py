b6cee0b5954325a6eb01891ccc648977ea0f8c60
Stephanie G <sswenson.anne@gmail.com>
2020-09-25 20:01:46 -0600

    Finished nonholoRobo

--- flightSimulationV2.py
+++ flightSimulationV2.py
@@ -2,6 +2,7 @@
 ###### A00979467 ######
 
 import json
+import time
 import sys, pygame, math, time
 import matplotlib.pyplot as plt
 
@@ -67,13 +68,11 @@ class HolonomicSim:
     def newPointFormula(self, betaRise):
         addToX = self.meterDiff*self.unit
         addToY = betaRise*self.unit
-        xDiff = self.destX - self.currentX
-        yDiff = self.destY - self.currentY
-        if (xDiff < addToX):
+        if abs(self.destX - self.currentX) < abs(addToX):
             self.currentX = self.destX
         else:
             self.currentX = self.currentX + addToX
-        if (yDiff < addToY):
+        if abs(self.destY - self.currentY) < abs(addToY):
             self.currentY = self.destY
         else:
             self.currentY = self.currentY + addToY
@@ -81,8 +80,9 @@ class HolonomicSim:
     def getDirection(self):
         yDiff = self.destY - self.originY
         xDiff = self.destX - self.originX
-        theta = math.atan2(yDiff, xDiff)
-        return math.degrees(theta)
+        thetaToX = math.atan2(yDiff, xDiff)
+        thetaToY = 90 - math.degrees(thetaToX) 
+        return thetaToY
 
     def newThetaFormula(self):
         if (self.currentX == self.destX and self.currentY == self.destY):
@@ -103,7 +103,7 @@ class HolonomicSim:
     def updateFlight(self):
         self.xPoints.append(self.currentX)
         self.yPoints.append(self.currentY)
-        print('X: {0} Y: {1}'.format(self.currentX, self.currentY))
+        print('HoloRobo - X: {0} Y: {1} Theta: {2}'.format(self.currentX, self.currentY, self.currentTheta))
         betaRise = self.riseByRun()
         self.newPointFormula(betaRise)
         self.newThetaFormula()
@@ -111,7 +111,7 @@ class HolonomicSim:
         if (self.arrived == True):
                 self.xPoints.append(self.destX)
                 self.yPoints.append(self.destY)
-                print('X: {0} Y: {1}'.format(self.currentX, self.currentY))
+                print('HoloRobo - X: {0} Y: {1} Theta: {2}'.format(self.currentX, self.currentY, self.currentTheta))
 
 class NonHolonomicSim:
     def __init__(self, originX, originY, theta, destX, destY, destTheta, maxVelocity, maxSteeringAngle):
@@ -126,48 +126,161 @@ class NonHolonomicSim:
         self.currentTheta = theta
         self.maxVelocity = maxVelocity
         self.maxSteeringAngle = maxSteeringAngle
+        self.xPoints = []
+        self.yPoints = []
+        self.arrived = False
+        self.goalTheta = 0
+        self.front = (0,0)
+        self.back = (0,0)
+        self.now = time.time()
+        self.arrived = False
+        self.currentTurnRadius = self.getTurnRadius()
+        self.addToX = 0
+        self.addToY = 0
 
-        def hasArrived(self):
-            if (self.currentX != self.destX):
-                return False
-            elif (self.currentY != self.destY):
-                return False
-            elif (self.currentTheta != destTheta):
-                return False
-            else:
-                return True
+        def getRise(self, originY, destY):
+            return destY - originY
+        
+        def getRun(self, originX, destX):
+            return destX - originX
+        
+        def getC(self, run, rise):
+            return math.sqrt((run*run) + (rise*rise))
+        
+        def getUnit(self, run, rise, c):
+            newRun = (run/2)*(run/2)
+            newRise = (rise/2)*(rise/2)
+            newValue = newRun + newRise
+            newC = (c/2)*(c/2)
+            return newC / newValue
+        
+        def metersPerFrame(maxVelocity):
+            return maxVelocity / FPS
+        
+        self.rise = getRise(self, originY, destY)
+        self.run = getRun(self, originX, destX)
+        self.c = getC(self, self.run, self.rise)
+        self.unit = getUnit(self, self.run, self.rise, self.c)
+        self.arrived = False
+        self.meterDiff = metersPerFrame(self.maxVelocity)
 
-        def updateFlight(self):
-            return "bla"
+    def riseByRun(self):
+        return self.rise*self.meterDiff/self.run
 
-        self.arrived = hasArrived(self)
+    def flyToNewPoint(self, betaRise):
+        self.addToX = self.meterDiff*self.unit
+        self.addToY = betaRise*self.unit
+        if abs(self.destX - self.currentX) < abs(self.addToX):
+            self.currentX = self.destX
+        else:
+            self.currentX = self.currentX + self.addToX
+        if abs(self.destY - self.currentY) < abs(self.addToY):
+            self.currentY = self.destY
+        else:
+            self.currentY = self.currentY + self.addToY
 
-class BadHolonomicSim:
-    def __init__(self, originX, originY, theta, destX, destY, destTheta):
-        self.originX = originX
-        self.originY = originY
-        self.theta = theta
-        self.destX = destX
-        self.destY = destY
-        self.destTheta = destTheta
-        self.currentX = originX
-        self.currentY = originY
-        self.currentTheta = theta
+    def updateFrontBack(self):
+        m = math.tan(self.currentTheta)
+        b = self.currentY - (m*self.currentX)
+        frontX = self.currentX + float(ROBO_LENGTH/2)
+        backX = self.currentX - float(ROBO_LENGTH/2)
+        frontY = m*frontX + b
+        backY = m*backX + b
+        print()
+        self.front = (frontX, frontY)
+        self.back = (backX, backY)
 
-        def hasArrived(self):
-            if (self.currentX != self.destX):
-                return False
-            elif (self.currentY != self.destY):
-                return False
-            elif (self.currentTheta != destTheta):
-                return False
-            else:
-                return True
-        self.arrived = hasArrived(self)
+    def crossProduct(self):
+        self.updateFrontBack()
+        pointA = ((self.currentX - self.front[0]), (self.currentY - self.front[1]))
+        pointB = ((self.destX - self.currentX), (self.destY - self.currentY))
+        return pointA[0]*pointB[1] - pointB[0]* pointA[1]
 
+    def hasArrived(self):
+        if (int(self.currentX) != int(self.destX)):
+            return False
+        elif (int(self.currentY) != int(self.destY)):
+            return False
+        elif (int(self.currentTheta) != int(destTheta)):
+            return False
+        else:
+            return True
 
-def metersPerFrame(maxVelocity):
-    return maxVelocity / FPS
+    def getTurnRadius(self):
+        return ROBO_LENGTH / 2 * math.sin(self.maxSteeringAngle)
+
+    def getDirection(self):
+        yDiff = self.destY - self.currentY
+        xDiff = self.destX - self.currentX
+        thetaToX = math.atan2(yDiff, xDiff)
+        thetaToY = 90 - math.degrees(thetaToX)
+        if thetaToY < 0:
+            thetaToY = thetaToY + 360
+        return thetaToY
+
+    def pickSteering(self):
+        cross = self.crossProduct()
+        if cross < 0:
+            return -1*self.maxSteeringAngle
+        else:
+            return self.maxSteeringAngle
+
+    def goalIsStraight(self):
+        self.goalTheta = self.getDirection()
+        if int(self.goalTheta) == int(self.currentTheta):
+            return True
+        else:
+            return False
+
+    def turnRobo(self):
+        alpha = self.pickSteering()
+        now = time.time()
+        deltaTime = (now - self.now)
+        newTheta = self.currentTheta + ((self.maxVelocity/ROBO_LENGTH)*math.tan(alpha)*deltaTime)
+        newX = self.currentX - (math.radians(math.sin(self.currentTheta))*deltaTime*self.maxVelocity)
+        newY = self.currentY + (math.radians(math.cos(self.currentTheta))*deltaTime*self.maxVelocity)
+        if abs(self.destX - self.currentX) < abs(newX - self.currentX):
+            newX = self.destX
+        if abs(self.destY - self.currentY) < abs(newY - self.currentY):
+            newY = self.destY
+        if abs(self.goalTheta - self.currentTheta) < abs(newTheta - self.currentTheta):
+            newTheta = self.goalTheta
+        self.currenX = newX
+        self.currentY = newY
+        self.currentTheta = newTheta
+
+    def updateTheta(self, addToX):
+        thetaDiff = abs(self.destTheta - self.currentTheta)
+        timesToUpdate = thetaDiff / self.maxSteeringAngle
+        xDiff = abs(self.destX - self.currentX)
+        unitsLeft = xDiff / addToX
+        if timesToUpdate != 0:
+            if unitsLeft <= timesToUpdate:
+                thetaUnit = thetaDiff / timesToUpdate
+                self.currentTheta = self.currentTheta + thetaUnit
+        else:
+            self.currentTheta = self.destTheta
+
+    def flyToGoal(self):
+        betaRise = self.riseByRun()
+        self.flyToNewPoint(betaRise)
+        self.updateTheta(self.addToX)
+        self.arrived = self.hasArrived()
+        if (self.arrived == True):
+                self.xPoints.append(self.destX)
+                self.yPoints.append(self.destY)
+
+    def updateFlight(self):
+        self.xPoints.append(self.currentX)
+        self.yPoints.append(self.currentY)
+        print('NonHoloRobo - X: {0} Y: {1} Theta: {2}'.format(self.currentX, self.currentY, self.currentTheta))   
+        if self.goalIsStraight():
+            self.goalTheta = destTheta
+            self.flyToGoal()
+        elif int(self.currentX) == int(self.destX) and int(self.currentY) == int(self.destY):
+            self.arrived = True
+        else:
+            self.turnRobo()
 
 def nonHoloRoboImg(x, y, theta):
     roboImg = pygame.image.load('nonHoloRobo2.jpg')
@@ -180,64 +293,6 @@ def holoRoboImg(x, y, theta):
     pygame.transform.scale(roboImg, (ROBO_LENGTH*SCREEN_SCALE, ROBO_LENGTH*SCREEN_SCALE))
     newRobo = pygame.transform.rotate(roboImg, theta)
     SCREEN_RENDER.blit(newRobo, (x, y))
-
-def badHoloRoboImg(x, y, theta):
-    roboImg = pygame.image.load('badRobos.jpg')
-    pygame.transform.scale(roboImg, (ROBO_LENGTH*SCREEN_SCALE, ROBO_LENGTH*SCREEN_SCALE))
-    newRobo = pygame.transform.rotate(roboImg, theta)
-    SCREEN_RENDER.blit(newRobo, (x, y))
-
-def getThetaG(maxVelocity):
-    return maxVelocity / ROBO_LENGTH * math.tan(ALPHA)
-
-def getXG(maxVelocity, theta):
-    return -1 * (maxVelocity / ROBO_LENGTH) * math.sin(theta)
-
-def getYG(maxVelocity, theta):
-    return maxVelocity * math.cos(theta)
-
-def newNonHoloPointFormula(nonHoloObj, maxVelocity, meterDiff):
-    xDiff = nonHoloObj.destX - nonHoloObj.currentX
-    yDiff = nonHoloObj.destY - nonHoloObj.currentY
-    if (xDiff < meterDiff):
-        nonHoloObj.currentX = destX
-    else:
-        nonHoloObj.currentX = nonHoloObj.currentX + getXG(maxVelocity, nonHoloObj.currentTheta) * meterDiff
-
-def getHoloOrientation(xDirection, yDirection, diff):
-    if (xDirection and diff < 0):
-        return 270
-    elif (xDirection and diff > 0):
-        return 90
-    elif (yDirection and diff < 0):
-        return 180
-    else:
-        return 0
-
-def newHoloPointFormula(holoObj, meterDiff):
-    xDiff = holoObj.destX - holoObj.currentX
-    yDiff = holoObj.destY - holoObj.currentY
-    moveX = False
-    moveY = False
-    diff = 0
-    if (xDiff < meterDiff):
-        holoObj.currentX = destX
-    else:
-        holoObj.currentX += meterDiff
-        moveX = True
-        diff = xDiff
-    if (holoObj.currentX == destX):
-        if(yDiff < meterDiff):
-            holoObj.currentY = destY
-        else:
-            holoObj.currentY += meterDiff
-            moveY = True
-            diff = yDiff
-    if (moveX == False and moveY == False):
-        holoObj.currentTheta = destTheta
-        holoObj.arrived = True
-    else:
-        holoObj.currentTheta = getHoloOrientation(moveX, moveY, diff)
 
 #############################
 #####  Get Config Info  #####
@@ -271,28 +326,33 @@ destTheta = distList[2]
 pygame.init()
 
 holoRobo = HolonomicSim(originX, originY, originTheta, destX, destY, destTheta, maxVelocity)
-#nonHoloRobo = 
+nonHoloRobo = NonHolonomicSim(originX, originY, originTheta, destX, destY, destTheta, maxVelocity, maxSteeringAngle)
 
 # First display with a wait so we can see original positions
 SCREEN_RENDER.fill(WHITE)
 holoRoboImg(int(holoRobo.currentX*SCREEN_SCALE), int(holoRobo.currentY*SCREEN_SCALE), holoRobo.currentTheta)
-#nonHoloRoboImg(int())
+nonHoloRoboImg(int(nonHoloRobo.currentX*SCREEN_SCALE), int(nonHoloRobo.currentY*SCREEN_SCALE), nonHoloRobo.currentTheta)
 pygame.display.set_caption('Flight Simulation')
 pygame.display.update()
 time.sleep(1)
 
-while (holoRobo.arrived == False):
+while (holoRobo.arrived == False or nonHoloRobo.arrived == False):
     holoRobo.updateFlight()
+    nonHoloRobo.updateFlight()
+    #time.sleep(1)
     SCREEN_RENDER.fill(WHITE)
     holoRoboImg(int(holoRobo.currentX*SCREEN_SCALE), int(holoRobo.currentY*SCREEN_SCALE), holoRobo.currentTheta)
+    nonHoloRoboImg(int(nonHoloRobo.currentX*SCREEN_SCALE), int(nonHoloRobo.currentY*SCREEN_SCALE), nonHoloRobo.currentTheta)
     pygame.display.update()
 
 time.sleep(1)
 
 pygame.quit()
 
-plt.plot(holoRobo.xPoints, holoRobo.yPoints)
+plt.xlim(0, 100)
+plt.ylim(0, 100)
 plt.xlabel('X Values', fontsize=18)
 plt.ylabel('Y Values', fontsize=16)
-#plt.plot(nonHoloExes, nonHoloWhys)
+plt.plot(holoRobo.xPoints, holoRobo.yPoints, "blue")
+plt.plot(nonHoloRobo.xPoints, nonHoloRobo.yPoints, "r-")
 plt.show()
